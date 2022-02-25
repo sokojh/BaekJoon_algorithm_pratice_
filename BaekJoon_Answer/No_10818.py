917708eecc321ca884b323962107352e4a587a78
jiho kim <sokojiho2@gmail.com>
2022-02-25 16:16:51 +0900

    TILpython

---
+++ BaekJoon_Answer/No_10818.py
@@ -0,0 +1,10 @@
+
+n = int(input())
+arr = str(input())
+num =0
+
+for i in range(n):
+    num +=int(arr[i])
+
+
+print(num)
---
+++ BaekJoon_Answer/No_10872.py
@@ -0,0 +1,5 @@
+
+arr = [1,2,4,5,6]
+a=0;
+for i in range(len(arr)):
+    print(arr[i])
