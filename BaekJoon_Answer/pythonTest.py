7ca46ea4d228e5d238b25c65a77047019072f10d
jiho kim <sokojiho2@gmail.com>
2022-03-12 14:59:11 +0900

    sk codding test

---
+++ BaekJoon_Answer/pythonTest.py
@@ -0,0 +1,12 @@
+import heapq
+import collections
+from collections import deque
+
+counter  = collections.Counter(["blue", "blue", "blue","banns",1,2,3,4])
+
+print(dict(counter))
+
+deque = deque([1,2,3,4])
+
+deque.append(1)
+deque.pop
---
+++ coddingTest/sk_coddingTest.py
@@ -0,0 +1,18 @@
+from itertools import combinations
+from itertools import permutations
+from itertools import product
+from itertools import combinations_with_replacement
+import heapq
+from collections import deque
+from collections import Counter
+import math
+money = 1999
+costs = [2, 11, 20, 100, 200, 600]
+
+
+
+def solution(money, costs):
+
+
+    answer = 0
+    return answer
