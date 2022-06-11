2243a7784132a3605d5e37c4886492d322178527
jiho kim <sokojiho2@gmail.com>
2022-06-11 21:26:09 +0900

    selfnumberAnswer

---
+++ inflearn_Answer/TS_01.py
@@ -0,0 +1,7 @@
+record = [5,'level']
+
+
+
+def solution(record):
+    answer = []
+    return answer
---
+++ inflearn_codding_Test/k번째_약수.py
@@ -0,0 +1,5 @@
+import sys
+sys.stdin=open("input.txt","rt") #파일 입출력을 위한
+n=map(input().split(" "))
+cnt =0
+print(n)
