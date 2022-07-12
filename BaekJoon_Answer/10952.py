d399dca78f2cbf2b96eb520be086cad1644dad49
jiho kim <sokojiho2@gmail.com>
2022-07-12 22:31:28 +0900

    feat:edit

--- BaekJoon_Answer/10952.py
+++ BaekJoon_Answer/10952.py
@@ -3,8 +3,4 @@ while True:
     if(A==0 and B == 0):
         break
     else:
-<<<<<<< HEAD
-        print(A+B+"ZZAb")
-=======
-        print(A+B+"ZZA")
->>>>>>> fc4c33fff15c785fa89284ae2bd1cb7fdbdc59ac
+        print('dd')
